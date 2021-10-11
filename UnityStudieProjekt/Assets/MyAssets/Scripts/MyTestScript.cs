7567b4700b45da285a0f6bf99d78583425a767bf
Fredrik <fredrik.persson@nackademin.se>
2021-10-11 17:19:23 +0200

    Upload lesson03

---
+++ UnityStudieProjekt/Assets/MyAssets/Scripts/MyTestScript.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyTestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        Debug.Log("Hello : this happens only once in the beginning");
+
+        Debug.Log("This is second message");
+
+        // Ändrar namnet på kubobjektet
+        transform.name = "SUPERCUBE";
+
+        transform.position = new Vector3(5.0f, 0.0f, 0.0f);
+
+        //transform.Translate(5.0f, 0.0f, 0.0f);
+
+        transform.Translate(1.0f, 0.0f, 0.0f);
+
+        transform.Rotate(0, 45, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //  transform.position = new Vector3(5.0f, 0.0f, 0.0f);
+
+        // transform.Translate(0.01f, 0.0f, 0.0f);
+
+        // Denna kommando skrivs ut varje frame per sekund
+        // Debug.Log("This happens every FPS");
+
+        // Debug.Log("Hello World");
+
+        /*
+         
+        if ( .....villkor..... ) { 
+            ........ 
+        } else { 
+            .......
+        }
+
+        */
+
+        // Detta är sättet för att läsa av tagentbordstryckningen "space"
+        if ( Input.GetKeyDown(KeyCode.Space) ) {
+            Debug.Log("Du tryckte ned space tagenten");
+        }
+    }
+
+
+    /*
+    void myFunction() { 
+        
+    }
+
+    */
+}
